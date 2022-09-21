fc5253b193297f20777af96319251f03a166d82a
Asher Hoeveler <contact@hecancanyou.xyz>
2022-09-21 03:47:07 -0500

    remove page cache for fast updating and add a second dropdown

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ function getCookie(name) {
 }
 
 document.body.onload = () => {
-    console.log(getCookie("DCID") ? getCookie(DCID) : "no dcid set");
+    console.log(getCookie("DCID") ? getCookie("DCID") : "no dcid set");
     document.title = getCookie("DCID");
 };
 
